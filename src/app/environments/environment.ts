25b2dd3c94b0a34818c1dfbbb6fb16b2532d8edf
judas <judas@iu.edu>
2025-12-10 23:07:37 -0500

    Added Edge Service Configuration

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: false,
-    groupApiUrl: 'http://localhost:8080/api',
-    individualApiUrl: 'http://localhost:8081/api',
-    userServiceUrl: 'http://localhost:8083',
-    schedulerServiceUrl: 'http://localhost:8085',
+    groupApiUrl: 'http://localhost:9000/api',
+    individualApiUrl: 'http://localhost:9000/api',
+    userServiceUrl: 'http://localhost:9000',
+    schedulerServiceUrl: 'http://localhost:9000',
     appName: 'Appointment Scheduler',
     version: '1.0.0'
 };
